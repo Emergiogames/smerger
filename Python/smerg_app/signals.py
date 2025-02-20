b5df1691ee801c68a92879090447439c4df0dff2
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-20 12:17:48 +0530

    refactor: streamline post update logic in Subscription signal handler

--- Python/smerg_app/signals.py
+++ Python/smerg_app/signals.py
@@ -42,14 +42,12 @@ def log_model_save(sender, instance, created, **kwargs):
 # Signal for updating posts after a subscription is brought
 @receiver(post_save, sender=Subscription)
 def log_model_save(sender, instance, created, **kwargs):
-    if created:
+    # updating unsubscribed posts
+    posts = SaleProfiles.objects.filter(user=instance.user, entity_type=instance.plan.type, subscribed=False).order_by('id')[:instance.remaining_posts]
+    post_ids = [post.id for post in posts]
+    if post_ids:
+        SaleProfiles.objects.filter(id__in=post_ids).update(subscribed=True)
 
-        # updating unsubscribed posts
-        posts = SaleProfiles.objects.filter(user=instance.user, entity_type=instance.plan.type, subscribed=False).order_by('id')[:instance.remaining_posts]
-        post_ids = [post.id for post in posts]
-        if post_ids:
-            SaleProfiles.objects.filter(id__in=post_ids).update(subscribed=True)
-
-        # updating remaining posts count
-        instance.remaining_posts = max(0, instance.remaining_posts - posts.count())
-        instance.save()
+    # updating remaining posts count
+    instance.remaining_posts = max(0, instance.remaining_posts - posts.count())
+    instance.save()
