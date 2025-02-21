df8081761cc8fff3925e01ed912c48a80d42ffb9
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-21 14:36:21 +0530

    refactor: set default value for rate field in Testimonial model

--- Python/smerg_app/models.py
+++ Python/smerg_app/models.py
@@ -195,7 +195,7 @@ class Suggestion(models.Model):
 class Testimonial(models.Model):
     user = models.ForeignKey(UserProfile,on_delete=models.CASCADE)
     company = models.CharField(max_length=1000)
-    rate = models.IntegerField()
+    rate = models.IntegerField(default=0)
     testimonial = models.TextField()
 
 # Log of datas
