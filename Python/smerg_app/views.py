a9ca16d1e7c4b8fa4d79bb49c8a0f0879268cd5a
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-22 10:12:00 +0530

    refactor: correct average rating calculation for advisors in Featured view

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -1084,7 +1084,7 @@ class Featured(APIView):
                     data.append(i)
         serialized_data = await serialize_data(data, serial)
         if request.GET.get('type') == "advisor":
-            rate = await sync_to_async(lambda: {advisor_.id: float(Testimonial.objects.filter(advisor=advisor_).aggregate(Avg('rate')) or 0) for advisor_ in product})()
+            rate = await sync_to_async(lambda: {advisor_.id: (Testimonial.objects.filter(advisor=advisor_).aggregate(Avg('rate')) or 0) for advisor_ in product})()
             # advisor_ratings = await rate()
             for advisor in serialized_data:
                 advisor['average_rating'] = rate.get(advisor['id'])['rate__avg']    
