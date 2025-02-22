94da2599a05705dc2b5e8255dcc99c9580bc13df
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-22 10:22:29 +0530

    refactor: update average rating calculation for advisors to use serialized data

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -541,7 +541,8 @@ class AdvisorList(APIView):
             if not exists:
                 return Response({'status':False,'message': 'User doesnot exist'}, status=status.HTTP_400_BAD_REQUEST)
             advisor = [posts async for posts in SaleProfiles.objects.filter(entity_type='advisor', user=user, block=False).order_by('-id')]
-        rate = await sync_to_async(lambda: {advisor_.id: (Testimonial.objects.filter(advisor=advisor_).aggregate(Avg('rate')) or 0) for advisor_ in advisor})()
+        serialized_data = await serialize_data(advisor, SaleProfilesSerial)
+        rate = await sync_to_async(lambda: {advisor_.id: (Testimonial.objects.filter(advisor=advisor_).aggregate(Avg('rate')) or 0) for advisor_ in product})()
         for advisor in serialized_data:
             advisor['average_rating'] = round(rate.get(advisor['id'])['rate__avg'], 1) if rate.get(advisor['id'])['rate__avg'] else 0
         return Response(serialized_data)
