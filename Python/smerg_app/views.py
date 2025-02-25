1de5b5dbd9219cac88789fb0e0f17e2865cfab32
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-25 14:06:55 +0530

    refactor: fix preference indexing in title generation for investor data

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -408,7 +408,7 @@ class InvestorList(APIView):
                 sale = data.get('type_sale', 'sale')
                 city = data.get('city', '...')
                 state = data.get('state', '...')
-                data["title"] = f"{industry}, {data.get("preference", ["Investment"][0])}, {city}, {state}"
+                data["title"] = f"{industry}, {data.get("preference", "Investment")[0]}, {city}, {state}"
                 data["single_desc"] = f'{data.get("preference", ["Investment"][0])} in {city}, {state}'
                 subscribed = await check_subscription(user, "investor")
                 if subscribed:
