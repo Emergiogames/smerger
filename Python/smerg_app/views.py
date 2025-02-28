29fafbccc76189eaf5c390f194fcd81517f309bf
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-02-28 13:40:24 +0530

    refactor: fix message count assignment in testimonials view

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -881,7 +881,7 @@ class Testimonials(APIView):
                     print(room)
                     if not room:
                         return Response({'status':False,'message': 'Chat not done'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-                    messages += await ChatMessage.objects.filter(room=room[0]).acount()
+                    messages = await ChatMessage.objects.filter(room=room[0]).acount()
                     if messages < 5:
                         return Response({'status':False,'message': 'Chat not done'}, status=status.HTTP_406_NOT_ACCEPTABLE)
                     await Testimonial.objects.acreate(user=user, advisor=advisor, rate=request.data.get('rate'), testimonial=request.data.get('testimonial'))
