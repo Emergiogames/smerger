2ff928e08ad97f5b105422ee8c0235cdcf27badd
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-03 13:03:19 +0530

    refactor: add json import for enhanced data handling in views

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -1,4 +1,4 @@
-import requests, aiohttp, asyncio, random, base64
+import requests, aiohttp, asyncio, random, base64, json
 from django.shortcuts import render
 # from rest_framework.views import APIView
 from adrf.views import APIView
