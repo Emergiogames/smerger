e5ed29d9b3a6d03e4f3f331a357b945601c831a2
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-05 07:19:53 +0530

    refactor: fix order data creation by using sync_to_async for create_order in RazorOrder view

--- Python/smerg_app/views.py
+++ Python/smerg_app/views.py
@@ -1056,7 +1056,7 @@ class RazorOrder(APIView):
                 if await Plan.objects.filter(id=request.GET.get('id')).aexists():
                     plan = await Plan.objects.aget(id=request.GET.get('id'))
                     order_amount = await sync_to_async(lambda: plan.rate)()
-                    order_data = await async_to_sync(create_order)(order_amount)
+                    order_data = await sync_to_async(create_order)(order_amount)
                     return Response(order_data)
                 return Response({'status':False,'message': 'Plan doesnot exist'}, status=status.HTTP_404_NOT_FOUND)
             return Response({'status':False,'message': 'User doesnot exist'}, status=status.HTTP_400_BAD_REQUEST)
