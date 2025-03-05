7a9ab317f9f5770b6ec242c50f093ba64e0eaa86
Muhammed Suhail <muhammedsuhusuhail@gmail.com>
2025-03-05 09:14:05 +0530

    refactor: add transaction key logging in verify_payment function

--- Python/smerg_app/utils/razorpay_utils.py
+++ Python/smerg_app/utils/razorpay_utils.py
@@ -19,11 +19,12 @@ from django.conf import settings
 
 def verify_payment(transaction_key, amount):
     print(f"Amount: {amount}")
+    print(f"Transaction Key: {transaction_key}")
     client = razorpay.Client(auth=(settings.RAZORPAY_API_KEY, settings.RAZORPAY_API_SECRET))
     amount_in_paise = int(int(amount) * 100)
     print(amount_in_paise)
     payment_details = client.payment.capture(transaction_key,{
-        "amount" : amount_in_paise,
+        "amount" : 100,
         "currency" : "INR"
     })
     print(payment_details)
